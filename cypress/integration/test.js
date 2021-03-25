57422b66da95bb4203724cc226655dbba1e0948f
Masha <mashushvik.2001@gmail.com>
2021-03-25 10:24:02 +0200

    update tests

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,52 +1,54 @@
 describe('tests lab3', () => {
-    it('find email input', () => {
-        cy.visit('https://www.pinterest.com/business/hub/?autologin=true');
+
+    it('form validation - skip email input', () => {
+        cy.visit('https://www.pinterest.com/');
+        cy.get("*[data-test-id=simple-login-button] button").click();
+        cy.get('#password').type("aaa");
+        cy.get("*[data-test-id=registerFormSubmitButton]").click();
+        cy.url().then(url => expect(url).to.equal("https://www.pinterest.com/")); // didn't redirect to user page
+    });
+
+    it('login with wrong password', () => {
+        cy.visit('https://www.pinterest.com/');
+        cy.get("*[data-test-id=simple-login-button] button").click();
+        cy.get("#email").type("19soaring.unicorns@gmail.com");
+        cy.get('#password').type("111");
+        cy.get("*[data-test-id=registerFormSubmitButton]").click();
+        cy.url().then(url => expect(url).to.equal("https://www.pinterest.com/")); // didn't redirect to user page
+    });
+
+    it('find contact page link', () => {
+        cy.visit('https://www.pinterest.com');
+        cy.get('a[href*=contact]').then((elem) => {
+            if (!elem) {
+                throw new Error("test fails here")
+            }
+        })
+    });
+
+    it('privacy policy / confidentiality links', () => {
+        cy.visit('https://www.pinterest.com');
         cy.get("button[data-test-id=\"page-scroll-arrow\"]:last-of-type").click();
-
-        cy.get('#email').then((elem) => {
+        cy.get('a[href*=terms-of-service]').then((elem) => {
+            if (!elem) {
+                throw new Error("test fails here")
+            }
+        });
+        cy.get('a[href*=privacy-policy]').then((elem) => {
             if (!elem) {
                 throw new Error("test fails here")
             }
         })
     });
 
-    it('find password input', () => {
-        cy.visit('https://www.pinterest.com/business/hub/?autologin=true');
-        cy.get("button[data-test-id=\"page-scroll-arrow\"]:last-of-type").click();
+    it('form autofocus on open', () => {
+        cy.visit('https://www.pinterest.com');
 
-        cy.get('input[name=password]').then((elem) => {
-            if (!elem) {
-                throw new Error("test fails here")
-            }
-        })
-    });
+        cy.get("*[data-test-id=simple-signup-button] button").click();
+        cy.focused().should('have.attr', 'id', 'email');
 
-    it('find submit button in sign in form', () => {
-        cy.visit('https://www.pinterest.com/business/hub/?autologin=true');
-        cy.get("button[data-test-id=\"page-scroll-arrow\"]:last-of-type").click();
-
-        cy.get("form[data-test-id=registerForm] *[data-test-id=registerFormSubmitButton] button").then((elem) => {
-            if (!elem) {
-                throw new Error("test fails here")
-            }
-        })
-    });
-
-    it('find about page link', () => {
-        cy.visit('https://www.pinterest.com/business/hub/?autologin=true');
-        cy.get('a[href*=about]').then((elem) => {
-            if (!elem) {
-                throw new Error("test fails here")
-            }
-        })
-    });
-
-    it('find button with red text', () => {
-        cy.visit('https://www.pinterest.com/business/hub/?autologin=true');
-        cy.get('button[style*="color:red"]').then((elem) => {
-            if (!elem) {
-                throw new Error("test fails here")
-            }
-        })
+        cy.get("*[data-test-id=full-page-signup-close-button] button").click();
+        cy.get("*[data-test-id=simple-login-button] button").click();
+        cy.focused().should('have.attr', 'id', 'email');
     });
 });
